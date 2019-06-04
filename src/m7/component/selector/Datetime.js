50c0803f9a8454a7331d8f0a35b30162a21bc35e
xlberry <xlberry@163.com>
2019-06-04 10:56:23 +0800

    [*]优化日期选择器数据传递和显示

--- src/m7/component/selector/Datetime.js
+++ src/m7/component/selector/Datetime.js
@@ -3,29 +3,29 @@
  */
 import React from "react";
 import PropTypes from "prop-types";
-import Element from "../../hoc/Element";
+import create from "../../hoc/WrapComponent";
 import Input from "../input/Input";
 import ProcessUtils from "../process/index";
 import DatetimeView from "./DatetimeView";
 
-@Element()
+@create({ type: "element" })
 export default class Datetime extends React.PureComponent {
   state = {
     userAction: false, // 交互变更，事件消耗
-    value: new Date()
+    data: null
   };
 
-  tmpData = new Date(); // 列变动过程变量
+  tmpData = null; // 列变动过程变量
 
   static getDerivedStateFromProps(props, state) {
     if (!state.userAction) {
       const { viewProxy, id, format } = props;
       const vData = viewProxy.get(id);
       if (vData instanceof Date) {
-        return { ...state, value: vData, data: vData["format"](format) };
+        return { ...state, data: vData, value: vData["format"](format) };
       } else {
         // 置空
-        return { ...state, value: new Date(), data: "" };
+        return { ...state, data: null, value: "" };
       }
     } else {
       return { ...state, userAction: false };
@@ -35,34 +35,46 @@ export default class Datetime extends React.PureComponent {
   componentDidUpdate(prevProps, prevState/*, snapshot*/) {
     if (prevState.data !== this.state.data) {
       const { id, onChange } = this.props;
-      typeof onChange === "function" && onChange({ id, data: this.state.data });
+      typeof onChange === "function" && onChange({ id, type: "datetime", data: this.state.data });
     }
   }
 
   onClick = () => {
-    const setValue = (value, reset = false) => {
+    const setStateData = (data, reset = false) => {
       const { viewProxy, id, format } = this.props;
-      id && viewProxy.set(id, value);
-      this.setState({ value, data: reset ? "" : value["format"](format), userAction: true });
+      this.tmpData = data;
+      this.setState({
+        data,
+        value: reset ? "" : data["format"](format),
+        userAction: true
+      }, () => {
+        id && viewProxy.set(id, data);
+      });
     };
 
+    let data; // 巧妙的空值获取当前最新时间
+    if (this.state.data === null) {
+      data = this.tmpData = new Date();
+    } else {
+      data = this.state.data;
+    }
     ProcessUtils.showPicker({
-      content: <DatetimeView format={this.props.format} onChange={this.pickOnChange} value={this.state.value}/>,
+      content: <DatetimeView format={this.props.format} onChange={this.handleOnChange} data={data}/>,
       contentCls: "m7-datetime__bd",
       cancelText: "清空",
-      cancel: () => setValue(new Date(), true),
+      cancel: () => setStateData(null, true),
       confirmText: "确定",
-      confirm: () => setValue(this.tmpData, false),
+      confirm: () => setStateData(this.tmpData, false),
     });
   };
 
-  pickOnChange = ({ year = 0, month = 0, day = 0, hour = 0, minute = 0, second = 0 }) => {
+  handleOnChange = ({ year = 0, month = 0, day = 0, hour = 0, minute = 0, second = 0 }) => {
     this.tmpData = new Date(year, month, day, hour, minute, second);
   };
 
   render() {
     const { id, title, placeholder, rules } = this.props;
-    return <Input id={id} title={title} placeholder={placeholder} value={this.state.data} rules={rules}
+    return <Input id={id} title={title} placeholder={placeholder} value={this.state.value} rules={rules}
                   onClick={this.onClick} readOnly subclass ft={<i className="m7-datetime__ft"/>}/>;
   }
 }
--- src/m7/component/selector/DatetimeView.js
+++ src/m7/component/selector/DatetimeView.js
@@ -2,117 +2,87 @@
  * Created by XLBerry on 2019/5/16
  */
 import React from "react";
-import { setRef, getRef } from "../../hoc/Enhance";
-import TouchEvent from "../../utils/TouchEvent";
+import create from "../../hoc/WrapComponent";
+import TransformTouch from "./TransformTouch";
+import { setTransform, setTransformMap } from "../../css/CSSPrefix";
 
-const pickerItemHeight = 34; // 元素高度px
-
+@create({ type: "element-view" })
 export default class DatetimeView extends React.Component {
   constructor(props) {
     super(props);
-    const { format } = props;
-    this.yearShow = format.indexOf("yyyy") !== -1;
-    this.monthShow = format.indexOf("MM") !== -1;
-    this.dayShow = format.indexOf("dd") !== -1;
-    this.hourShow = format.indexOf("hh") !== -1;
-    this.minuteShow = format.indexOf("mm") !== -1;
-    this.secondShow = format.indexOf("ss") !== -1;
-
-    this.state = {
-      year: {},
-      month: {},
-      day: {},
-      hour: {},
-      minute: {},
-      second: {}
-    };
-    this.init(this.state);
-  }
-
-  setRef = setRef.value.bind(this);
-
-  getRef = getRef.value.bind(this);
-
-  init(state) {
-    const { sTime = new Date(1900, 0, 1, 0, 0, 0), value } = this.props;
-    let maxDay = new Date(value.getFullYear(), value.getMonth() + 1, 0);
-    if (this.yearShow) {
-      this.initNums(state, "year", sTime.getFullYear(), maxDay.getFullYear() + 10, value.getFullYear());
+    const { format, sTime = new Date(1900, 0, 1, 0, 0, 0), data } = props,
+      maxDay = new Date(data.getFullYear(), data.getMonth() + 1, 0);
+    this.state = {};
+    if (format.indexOf("yyyy") !== -1) {
+      this.state.year = {};
+      this.initNums(this.state, "year", sTime.getFullYear(), maxDay.getFullYear() + 10, data.getFullYear());
     }
-    if (this.monthShow) {
-      this.initNums(state, "month", 0, 12, value.getMonth());
+    if (format.indexOf("MM") !== -1) {
+      this.state.month = {};
+      this.initNums(this.state, "month", 0, 12, data.getMonth());
     }
-    if (this.dayShow) {
-      state.day.tag = `${state.year.value}/${state.month.value}`;
-      this.initNums(state, "day", 1, maxDay.getDate(), value.getDate());
+    if (format.indexOf("dd") !== -1) {
+      this.state.day = {};
+      this.state.day.tag = `${this.state.year.value}/${this.state.month.value}`;
+      this.initNums(this.state, "day", 1, maxDay.getDate(), data.getDate());
     }
-    if (this.hourShow) {
-      this.initNums(state, "hour", 0, 23, value.getHours());
+    if (format.indexOf("hh") !== -1) {
+      this.state.hour = {};
+      this.initNums(this.state, "hour", 0, 23, data.getHours());
     }
-    if (this.minuteShow) {
-      this.initNums(state, "minute", 0, 59, value.getMinutes());
+    if (format.indexOf("mm") !== -1) {
+      this.state.minute = {};
+      this.initNums(this.state, "minute", 0, 59, data.getMinutes());
     }
-    if (this.secondShow) {
-      this.initNums(state, "second", 0, 59, value.getSeconds());
+    if (format.indexOf("ss") !== -1) {
+      this.state.second = {};
+      this.initNums(this.state, "second", 0, 59, data.getSeconds());
     }
   }
 
+  /** 设置列显示内容 */
   initNums = (state, type, start, end, value) => {
     state[type].start = start;
     state[type].value = value;
     state[type].count = end;
-    state[type].translateY = (value - start) * pickerItemHeight;
-    let nums = [];
+    state[type].translateY = (value - start) * this.itemHeight;
+    state[type].nums = [];
     for (let i = type === "month" ? start + 1 : start; i <= end; i++) {
-      nums.push(<div key={i} className="m7-datetime__item">{i}</div>);
+      state[type].nums.push(<div key={i} className="m7-datetime__item">{i}</div>);
     }
-    state[type].nums = <div className="m7-datetime__content"
-                            style={{ transform: `translate3d(0px, -${state[type].translateY}px, 0px)` }}>{nums}</div>;
   };
 
+  itemHeight = 0; // 元素高度px
+
   componentDidMount() {
-    this.setTouch("year");
-    this.setTouch("month");
-    this.setTouch("day");
-    this.setTouch("hour");
-    this.setTouch("minute");
-    this.setTouch("second");
+    let state = this.state;
+    Object.keys(state).forEach((type) => {
+      this.itemHeight = this.itemHeight || this.getRef(type).children[0].clientHeight;
+      state[type].translateY = (state[type].value - state[type].start) * this.itemHeight;
+      this[`${type}Touch`] = new TransformTouch({
+        target: this.getRef(type),
+        callback: this.touchCallback.bind(this, type)
+      });
+    });
+    this.setState(state);
   }
 
   componentWillUnmount() {
-    this.setTouch("year", true);
-    this.setTouch("month", true);
-    this.setTouch("day", true);
-    this.setTouch("hour", true);
-    this.setTouch("minute", true);
-    this.setTouch("second", true);
+    Object.keys(this.state).forEach((type) => this[`${type}Touch`].unBind());
   }
 
-  setTouch = (type, unBind = false) => {
-    if (!this[`${type}Show`]) return;
-    if (unBind) {
-      this[`${type}Touch`].unBind();
-    } else {
-      this[`${type}Touch`] = new TouchEvent({
-        container: this.getRef(type), target: this.getRef(type).children[2],
-        callback: this.touchCallback.bind(this, type)
-      });
+  touchCallback = (type, { clickNode, translateY: x }) => {
+    let state = this.state;
+    if (clickNode) {
+      // 点击事件，先执行滚动定位
+      x += (state[type].value - (~~clickNode.innerText) + (type === "month" ? 1 : 0)) * this.itemHeight;
+      this[`${type}Touch`].setTranslate({ translateY: ~~x });
     }
-  };
-
-  touchCallback = (type, { translateY: x }) => {
-    let value = Math.round(Math.abs(x) / pickerItemHeight) + 1, state = this.state;
-    this[`${type}Touch`].setTranslate({ translateY: -(value - 1) * pickerItemHeight });
-    if (type === "year") {
-      state[type].value = state[type].start + value - 1;
-    } else if (type === "month") {
-      state[type].value = value - 1;
-    } else {
-      state[type].value = value;
-    }
-    if (this.dayShow && (type === "year" || type === "month")) {
-      // 触发日期天数变动
-      let dayTag = state.day.tag;
+    let value = Math.round(Math.abs(x) / this.itemHeight) + 1;
+    this[`${type}Touch`].setTranslate({ translateY: -(value - 1) * this.itemHeight }); // 嵌入
+    state[type].value = state[type].start + value - 1;
+    if (this.state.day && (type === "year" || type === "month")) {
+      let dayTag = state.day.tag; // 触发日期天数变动
       state.day.tag = `${state.year.value}/${state.month.value}`;
       if (dayTag !== this.state.day.tag) {
         // 触发天数列渲染
@@ -120,53 +90,27 @@ export default class DatetimeView extends React.Component {
         let dayNumSize = d.getDate();
         if (state.day.count !== dayNumSize) {
           this.initNums(state, "day", 1, dayNumSize, state.day.value > dayNumSize ? dayNumSize : state.day.value);
-          this.setState(state);
+          this.setState(state, () => {
+            setTransform(this.getRef("day"), "0px", `-${this.state.day.translateY}px`);
+          });
         }
       }
     }
-
-    typeof this.props.onChange === "function" && this.props.onChange({
-      year: this.state.year.value,
-      month: this.state.month.value,
-      day: this.state.day.value,
-      hour: this.state.hour.value,
-      minute: this.state.minute.value,
-      second: this.state.second.value,
-    });
+    let result = {};
+    Object.keys(this.state).forEach((type) => result[type] = this.state[type].value);
+    typeof this.props.onChange === "function" && this.props.onChange(result);
   };
 
   render() {
-    return [
-      this.yearShow ? <div key="year" className="m7-datetime__group" ref={this.setRef("year")}>
+    return Object.keys(this.state).map((type) => {
+      return <div key={type} className="m7-datetime__group">
         <div className="m7-datetime__mask"/>
         <div className="m7-datetime__indicator"/>
-        {this.state.year.nums}
-      </div> : null,
-      this.monthShow ? <div key="month" className="m7-datetime__group" ref={this.setRef("month")}>
-        <div className="m7-datetime__mask"/>
-        <div className="m7-datetime__indicator"/>
-        {this.state.month.nums}
-      </div> : null,
-      this.dayShow ? <div key="day" className="m7-datetime__group" ref={this.setRef("day")}>
-        <div className="m7-datetime__mask"/>
-        <div className="m7-datetime__indicator"/>
-        {this.state.day.nums}
-      </div> : null,
-      this.hourShow ? <div key="hour" className="m7-datetime__group" ref={this.setRef("hour")}>
-        <div className="m7-datetime__mask"/>
-        <div className="m7-datetime__indicator"/>
-        {this.state.hour.nums}
-      </div> : null,
-      this.minuteShow ? <div key="minute" className="m7-datetime__group" ref={this.setRef("minute")}>
-        <div className="m7-datetime__mask"/>
-        <div className="m7-datetime__indicator"/>
-        {this.state.minute.nums}
-      </div> : null,
-      this.secondShow ? <div key="second" className="m7-datetime__group" ref={this.setRef("second")}>
-        <div className="m7-datetime__mask"/>
-        <div className="m7-datetime__indicator"/>
-        {this.state.second.nums}
-      </div> : null,
-    ];
+        <div ref={this.setRef(type)} className="m7-datetime__content"
+             style={setTransformMap("0px", `-${this.state[type].translateY}px`)}>
+          {this.state[type].nums}
+        </div>
+      </div>;
+    });
   }
 }
