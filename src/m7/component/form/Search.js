680e05c1fb27004fde982c64833c4d8b8a110cb4
xlberry <xlberry@163.com>
2019-06-04 10:51:34 +0800

    [+]搜索框组件新增表单提交逻辑

--- src/m7/component/form/Search.js
+++ src/m7/component/form/Search.js
@@ -4,7 +4,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default class Search extends React.PureComponent {
+export default class Search extends React.Component {
 
   state = {
     data: "",
@@ -13,7 +13,8 @@ export default class Search extends React.PureComponent {
 
   componentDidUpdate(prevProps, prevState/*, snapshot*/) {
     if (prevState.data !== this.state.data) {
-      typeof this.props.onChange === "function" && this.props.onChange({ data: this.state.data });
+      const { id, onChange } = this.props;
+      typeof onChange === "function" && onChange({ id, type: "search", data: this.state.data });
     }
   }
 
@@ -23,7 +24,7 @@ export default class Search extends React.PureComponent {
 
   /** 点击取消 */
   handleInputCancel = (e) => {
-    console.log("handleInputCancel");
+    if (this.props.disabled) return;
     let inputDOM = e.target.parentNode.children[0].getElementsByTagName("input")[0];
     inputDOM.value = "";
     inputDOM.focus();
@@ -32,6 +33,7 @@ export default class Search extends React.PureComponent {
 
   /** 输入框焦点离开 */
   handleInputBlur = (e) => {
+    if (this.props.disabled) return;
     let inputDOM = e.target;
     setTimeout(() => {
       let value = inputDOM.value, focusing = !!value || !!inputDOM.tag;
@@ -42,6 +44,7 @@ export default class Search extends React.PureComponent {
 
   /** 输入框内容文字清除 */
   handleInputClear = (e) => {
+    if (this.props.disabled) return;
     let inputDOM = e.target.parentNode.getElementsByTagName("input")[0];
     inputDOM.value = "";
     inputDOM.tag = "clear";
@@ -50,17 +53,26 @@ export default class Search extends React.PureComponent {
 
   /** 默认标签点击触发 */
   handleLabelClick = (e) => {
+    if (this.props.disabled) return;
     e.target.parentNode.children[0].getElementsByTagName("input")[0].focus(); // 焦点聚焦输入框
     this.setState({ focusing: true });
+  };
+
+  /** 软键盘触发搜索 */
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({}, () => {
+      document.activeElement.blur();
+    });
   };
 
   render() {
     const { focusing } = this.state;
     return <div className={`m7-search-bar ${focusing ? "m7-search-bar_focusing" : ""}`}>
-      <form className="m7-search-bar__form">
+      <form className="m7-search-bar__form" onSubmit={this.handleSubmit}>
         <div className="m7-search-bar__box">
           <i className="weui-icon-search"/>
-          <input type="search" className="m7-search-bar__input" placeholder="搜索" required onBlur={this.handleInputBlur}/>
+          <input type="search" className="m7-search-bar__input" placeholder="搜索" required disabled={this.props.disabled} onBlur={this.handleInputBlur}/>
           <a href="javascript:" className="weui-icon-clear" onClick={this.handleInputClear}/>
         </div>
         <label className="m7-search-bar__label" onClick={this.handleLabelClick}>
@@ -75,6 +87,6 @@ export default class Search extends React.PureComponent {
 
 Search.propTypes = {
   ...Search.propTypes,
-
+  disabled: PropTypes.bool,
   onChange: PropTypes.func
 };
