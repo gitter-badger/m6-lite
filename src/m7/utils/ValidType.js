90735ffae68b24c495108a3085867c298c08c025
xlberry <xlberry@163.com>
2019-06-10 17:55:57 +0800

    [+]添加本地化表单校验配置

---
+++ src/m7/utils/ValidType.js
@@ -0,0 +1,16 @@
+/**
+ * Created by XLBerry on 2019/6/6
+ */
+
+export default {
+  required(value) {
+    if (Array.isArray(value)) {
+      return value.length > 0;
+    } else if (typeof value === 'object') {
+      return Object.keys(value).length > 0;
+    } else return !!(value && value !== 0);
+  },
+  mobile(value) {
+    return typeof (value) === 'string' && /^1[3456789]\d{9}$/.test(value.trim());
+  },
+};
